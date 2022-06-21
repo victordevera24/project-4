c6805900e3bdfcdf26e3765a96dee216f8719bb8
Victor De Vera <36523395+victordevera24@users.noreply.github.com>
2022-06-21 10:16:50 -0700

    uncommented bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
-// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 
